978e7be8ec8e843d90451bc2f927fd3aac87697a
Aaron Baker <aaron.baker@channelgrabber.com>
2019-05-07 13:43:31 +0100

    Corrected config/autoload/di.global.php to work with the latest channelgrabber/di

--- config/autoload/di.global.php
+++ config/autoload/di.global.php
@@ -1,13 +1,15 @@
 <?php
 use CG\Di\Definition\CacheDefinition;
 use CG\Di\DefinitionList;
-use Zend\Cache\Storage\StorageInterface;
+use CG\Di\Di;
 use Zend\Config\Config as ZendConfig;
 use Zend\Db\Adapter\Adapter;
 use Zend\Di\Config;
-use Zend\Di\Di;
+use Zend\Di\Definition\ClassDefinition;
+use Zend\Di\Di as ZendDi;
 use Zend\Di\InstanceManager;
 use Zend\Di\LocatorInterface;
+use Zend\EventManager\EventManagerInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\ServiceManager;
 
@@ -15,27 +17,23 @@ return [
     'service_manager' => [
         'factories' => [
             Di::class => function(ServiceLocatorInterface $serviceManager) {
-                /**
-                 * @var $configuration array
-                 */
+                /** @var $configuration array */
                 $configuration = $serviceManager->get('config');
 
-                /**
-                 * @var $cache StorageInterface
-                 */
-                $cache = $serviceManager->get('DiCacheStorage');
-
                 $runtimeDefinition = new CacheDefinition(
-                    null,
-                    require dirname(dirname(__DIR__)) . '/vendor/composer/autoload_classmap.php',
-                    $cache
+                    $introspectionStrategy = null,
+                    $explicitClasses = (require dirname(dirname(__DIR__)) . '/vendor/composer/autoload_classmap.php'),
+                    $cachePrefix = (ENVIRONMENT !== 'dev' ? $configuration['application_name'] ?? null : null)
                 );
 
-                $definitionList = new DefinitionList([$runtimeDefinition->toArrayDefinition(), $runtimeDefinition]);
+                $definitionList = new DefinitionList([$runtimeDefinition]);
                 $im = new InstanceManager();
-                $config = new Config(isset($configuration['di']) ? $configuration['di'] : []);
+                $config = new Config($configuration['di'] ?? []);
 
                 $di = new Di($definitionList, $im, $config);
+                $di->definitions()->unshift(
+                    (new ClassDefinition(\Memcached::class))->addMethodParameter('__construct', 'persistent_id', ['required' => false])
+                );
 
                 if (isset($configuration['db'], $configuration['db']['adapters'])) {
                     foreach (array_keys($configuration['db']['adapters']) as $adapter) {
@@ -45,15 +43,20 @@ return [
                 }
 
                 $im->addSharedInstance($di, Di::class);
+                $im->addSharedInstance($di, ZendDi::class);
                 $im->addSharedInstance($serviceManager, ServiceManager::class);
                 $im->addSharedInstance($di->get('config', array('array' => $configuration)), 'config');
                 $im->addSharedInstance($di->get(ZendConfig::class, array('array' => $configuration)), 'app_config');
 
                 return $di;
             },
+            ZendDi::class => function(ServiceLocatorInterface $serviceManager) {
+                return $serviceManager->get(Di::class);
+            }
         ],
         'shared' => [
             Di::class => true,
+            ZendDi::class => true,
         ],
         'aliases' => [
             'Di' => Di::class,
@@ -67,6 +70,7 @@ return [
                 'app_config' => ZendConfig::class,
             ],
             'preferences' => [
+                ZendDi::class => Di::class,
                 LocatorInterface::class => Di::class,
             ],
         ],
