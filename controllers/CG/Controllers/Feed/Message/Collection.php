11afdc98b4af4f427c5a4e23e8769de65c5c661e
Aaron Baker <aaron.baker@channelgrabber.com>
2019-05-10 16:49:19 +0100

    PTNR-1: added some missing use declarations

--- controllers/CG/Controllers/Feed/Message/Collection.php
+++ controllers/CG/Controllers/Feed/Message/Collection.php
@@ -3,8 +3,11 @@ namespace CG\Controllers\Feed\Message;
 
 use CG\Feed\Message\Filter;
 use CG\Feed\Message\RestService;
+use CG\Http\Exception\Exception4xx\NotFound as HttpNotFound;
+use CG\Permission\Exception as PermissionException;
 use CG\Slim\Controller\Collection\GetTrait;
 use CG\Slim\ControllerTrait;
+use CG\Stdlib\Exception\Runtime\NotFound;
 use Slim\Slim;
 use Zend\Di\Di;
 
